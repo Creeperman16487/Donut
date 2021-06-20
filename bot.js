4c00e72f5f3ee798ffe3e22fe377aa36d0f56768
Creeperman16487 <86112078+Creeperman16487@users.noreply.github.com>
2021-06-20 08:34:33 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,61 @@
-
-                let Discord;
-                let Database;
-                if(typeof window !== "undefined"){
-                    Discord = DiscordJS;
-                    Database = EasyDatabase;
-                } else {
-                    Discord = require("discord.js");
-                    Database = require("easy-json-database");
-                }
-                const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
-                const s4d = {
-                    Discord,
-                    client: null,
-                    tokenInvalid: false,
-                    reply: null,
-                    joiningMember: null,
-                    database: new Database("./db.json"),
-                    checkMessageExists() {
-                        if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
-                        if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
-                    }
-                };
-                s4d.client = new s4d.Discord.Client({
-                    fetchAllMembers: true
-                });
-                s4d.client.on('raw', async (packet) => {
-                    if(['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)){
-                        const guild = s4d.client.guilds.cache.get(packet.d.guild_id);
-                        if(!guild) return;
-                        const member = guild.members.cache.get(packet.d.user_id) || guild.members.fetch(d.user_id).catch(() => {});
-                        if(!member) return;
-                        const channel = s4d.client.channels.cache.get(packet.d.channel_id);
-                        if(!channel) return;
-                        const message = channel.messages.cache.get(packet.d.message_id) || await channel.messages.fetch(packet.d.message_id).catch(() => {});
-                        if(!message) return;
-                        s4d.client.emit(packet.t, guild, channel, message, member, packet.d.emoji.name);
-                    }
-                });
-                s4d.client.login(process.env.TOKEN).catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+let Discord;
+let Database;
+if (typeof window !== "undefined") {
+    Discord = DiscordJS;
+    Database = EasyDatabase;
+} else {
+    Discord = require("discord.js");
+    Database = require("easy-json-database");
+}
+const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+const s4d = {
+    Discord,
+    client: null,
+    tokenInvalid: false,
+    reply: null,
+    joiningMember: null,
+    database: new Database("./db.json"),
+    checkMessageExists() {
+        if (!s4d.client) throw new Error('You cannot perform message operations without a Discord.js client')
+        if (!s4d.client.readyTimestamp) throw new Error('You cannot perform message operations while the bot is not connected to the Discord API')
+    }
+};
+s4d.client = new s4d.Discord.Client({
+    fetchAllMembers: true
+});
+s4d.client.on('raw', async (packet) => {
+    if (['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(packet.t)) {
+        const guild = s4d.client.guilds.cache.get(packet.d.guild_id);
+        if (!guild) return;
+        const member = guild.members.cache.get(packet.d.user_id) || guild.members.fetch(d.user_id).catch(() => {});
+        if (!member) return;
+        const channel = s4d.client.channels.cache.get(packet.d.channel_id);
+        if (!channel) return;
+        const message = channel.messages.cache.get(packet.d.message_id) || await channel.messages.fetch(packet.d.message_id).catch(() => {});
+        if (!message) return;
+        s4d.client.emit(packet.t, guild, channel, message, member, packet.d.emoji.name);
+    }
+});
+s4d.client.login('ODU1OTU1NjcyNzI1MzIzNzg1.YM6A1w.nex1mqyyejkQUpAL68QZg7XE1I4').catch((e) => {
+    s4d.tokenInvalid = true;
+    s4d.tokenError = e;
+});
 
 s4d.client.on('ready', async () => {
-  s4d.client.user.setActivity(String('lol'));
-  console.log("JOined")
+    s4d.client.user.setActivity(String('lol'));
+
 });
 
 s4d.client.on('message', async (s4dmessage) => {
-  if ((s4dmessage.content) == ';help') {
-    console.log("got message")
-    (s4dmessage.author).send(
-            {
-                embed: {
-                    title: ';help',
-                    color: null,
-                    fields: [
+    if ((s4dmessage.content) == ';help') {
+        (s4dmessage.member).send({
+            embed: {
+                title: 'Help',
+                color: null,
+                image: {
+                    url: null
+                },
+								fields: [
                       {
                         name: 'Links',
                         value: "Are you looking for ban appeals, User & Bug Reports, Partner Applications, our Twitter, Roblox group or any sort of link? Head over to #info and take a look at 'Links'.",
@@ -79,16 +82,18 @@ s4d.client.on('message', async (s4dmessage) => {
                         value: 'Have a common or simple question that you want to be answered? Type ;faq in #bot-commands.',
                       },
                     ],
-                    image: { url: null },
-
-                    description: '',
-                    footer: { text: "Awes0me Studios" },
-                    thumbnail: { url: null }
+                description: null,
+                footer: {
+                    text: 'Awesome Studios'
+                },
+                thumbnail: {
+                    url: null
                 }
+
             }
-        );
-  }
+        });
+    }
 
 });
+
 s4d;
-            
